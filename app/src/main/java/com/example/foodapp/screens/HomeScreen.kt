a80b54b68cfa536268f61443c12ce98be0cf41ff
Greys2004 <gav77465@lasallebajio.edu.mx>
2025-03-05 00:47:38 -0600

    Categorias

--- app/src/main/java/com/example/foodapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/foodapp/screens/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.example.foodapp.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +22,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.foodapp.components.categoriasCards
+import com.example.foodapp.models.categoriasLista
 import com.example.foodapp.ui.theme.myColor
 import com.example.foodapp.utils.Account_circle
 import com.example.foodapp.utils.Logout
@@ -53,6 +59,17 @@ fun HomeScreen(innerPadding: PaddingValues){
             )
         }
 
-        Text(text= "Nuestras Categorias")
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text= "Nuestras Categorias", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+
+        LazyRow(
+            modifier = Modifier.fillMaxWidth().padding(top = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+
+        ) {
+            items(categoriasLista){cat ->
+                categoriasCards(categorias = cat )
+            }
+        }
     }
 }
